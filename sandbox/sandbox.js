63571c3716655507282b03de400d006f85794b41
Nikita Kozlov <kozlovzxc@gmail.com>
2018-02-14 20:55:58 +0300

    add basic sandbox on casper.js

---
+++ sandbox/sandbox.js
@@ -0,0 +1,27 @@
+const casper = require('casper').create();
+casper.options.viewportSize = { width: 1920, height: 1080 };
+
+function Report(url, message){
+    this.url = url;
+    this.message = message;
+}
+
+
+const args = casper.cli.args;
+if (args.length === 0 || args.length > 1) {
+    console.error("There must be exactly one url");
+}
+
+const target = args[0];
+const reports = [];
+casper.on('remote.alert', function(message) {
+    reports.push(new Report(
+        this.getCurrentUrl(),
+        message
+    ));
+});
+casper.on('run.complete', function() {
+    this.echo(JSON.stringify(reports));
+})
+casper.start(target);
+casper.run();
