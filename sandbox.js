4006a4c832cdd84fe48e6d383d2d56f43ea12f18
Nikita Kozlov <kozlovzxc@gmail.com>
2018-05-01 12:50:16 +0300

    add url parsing

--- sandbox.js
+++ sandbox.js
@@ -1,29 +1,41 @@
+const parse = require('url-parse');
 const casper = require('casper').create();
 casper.options.viewportSize = { width: 1920, height: 1080 };
 
-function Report(url, cookies, message){
-  this.url = url;
-  this.cookies = cookies;
-  this.message = message;
+function report(url, cookies, message){
+  return {
+    url: url,
+    cookies: cookies,
+    message: message
+  };
 }
 
+function main(args) {
+  const url = parse(args[0], true);
+  console.log(JSON.stringify(url));
+
+  const target = args[0];
+  const reports = [];
+
+  casper.on('remote.alert', function(message) {
+    reports.push(report(
+      this.getCurrentUrl(),
+      this.page.cookies,
+      message
+    ));
+  });
+
+  casper.on('run.complete', function() {
+    this.echo(JSON.stringify(reports, null, 2));
+  });
+
+  casper.start(target);
+  casper.run();
+}
 
 const args = casper.cli.args;
 if (args.length === 0 || args.length > 1) {
   console.error('There must be exactly one url');
+} else {
+  main(args);
 }
-
-const target = args[0];
-const reports = [];
-casper.on('remote.alert', function(message) {
-  reports.push(new Report(
-    this.getCurrentUrl(),
-    this.page.cookies,
-    message
-  ));
-});
-casper.on('run.complete', function() {
-  this.echo(JSON.stringify(reports));
-});
-casper.start(target);
-casper.run();
